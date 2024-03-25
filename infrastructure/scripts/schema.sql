be8a14f7a2d2500ff3b8b7f165a6da9074692d3f
toarga <oarga.tamas@3dmap.hu>
2024-03-25 16:33:51 +0100

    schema adjustments ( primary key, dbengine definition, nullable columns... )

--- infrastructure/scripts/schema.sql
+++ infrastructure/scripts/schema.sql
@@ -4,14 +4,14 @@ GRANT ALL PRIVILEGES ON incubeta_ads.* TO 'incubeta_ads'@'%';
 
 
 CREATE TABLE incubeta_ads.fare_prices(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     route VARCHAR(15) NOT NULL,
-    org_country_code VARCHAR(3) NOT NULL,
+    org_country_code VARCHAR(3) NULL,
     org_country_name VARCHAR(100) NOT NULL,
     org_station_code VARCHAR(5) NOT NULL,
     org_station_code2 VARCHAR(5) NOT NULL,
     org_city_name VARCHAR(100) NOT NULL,
-    dst_country_code  VARCHAR(3)NOT NULL,
+    dst_country_code  VARCHAR(3) NULL,
     dst_country_name  VARCHAR(100)NOT NULL,
     dst_station_code  VARCHAR(5)NOT NULL,
     dst_station_code2 VARCHAR(5) NOT NULL,
@@ -21,10 +21,9 @@ CREATE TABLE incubeta_ads.fare_prices(
     lowest_fare_premium_oneway INT NOT NULL DEFAULT 0,
     lowest_fare_economy_return INT NOT NULL DEFAULT 0,
     lowest_fare_premium_return INT NOT NULL DEFAULT 0,
-    destination_url_economy_oneway VARCHAR(255) NOT NULL,
-    destination_url_premium_oneway VARCHAR(255) NOT NULL,
-    destination_url_economy_return VARCHAR(255) NOT NULL,
-    destination_url_premium_return VARCHAR(255) NOT NULL,
-    destination_city_image_url VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
+    destination_url_economy_oneway VARCHAR(255) NULL,
+    destination_url_premium_oneway VARCHAR(255) NULL,
+    destination_url_economy_return VARCHAR(255) NULL,
+    destination_url_premium_return VARCHAR(255) NULL,
+    destination_city_image_url VARCHAR(255) NULL
+) ENGINE=InnoDB;
